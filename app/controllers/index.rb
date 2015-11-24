5db6c2cefbccf021aa181aea65e670b8fced135c
codeamac01 <codeamac01@gmail.com>
2015-11-24 13:38:54 -0600

    Agregando vista de Poll con opciones asignadas a cada pregunta

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -22,6 +22,14 @@ before '/survey/user/:user_id' do
 	@user = User.find(session[:user_id])
 end 
 
+before '/survey/poll_survey' do 
+	unless session[:user_id]
+	  session[:errors] = "No tienes una sesión por favor inicia sesión"
+		redirect ("/")
+	end
+	@user = User.find(session[:user_id])
+end 
+
 get '/' do
   # La siguiente linea hace render de la vista 
   # que esta en app/views/index.erb
@@ -40,6 +48,16 @@ get '/create' do
   erb :'survey/survey_title'
 end
 
+get '/survey/poll_survey' do
+	@survey = Survey.find(1)
+	@questions = @survey.questions
+	@options = Option.where(survey_id: @survey.id)
+	puts "*" * 50
+	p @questions
+	p @options
+  erb :'survey/poll_survey'
+end
+
 post '/survey/user/:user_id' do
   @survey = Survey.create(title: params["title"])
   @survey_id = Survey.last.id
@@ -51,8 +69,8 @@ post '/create/user/:user_id/survey/:survey_id' do
   puts "*" * 50
 	puts params
 	@question = Question.create(question: params["question"], survey_id: params[:survey_id])
-  @option1 = Option.create(option: params["option1"], question_id: @question.id)
-  @option2 = Option.create(option: params["option2"], question_id: @question.id)
-  @option3 = Option.create(option: params["option3"], question_id: @question.id)
+  @option1 = Option.create(option: params["option1"], question_id: @question.id, survey_id: params[:survey_id])
+  @option2 = Option.create(option: params["option2"], question_id: @question.id, survey_id: params[:survey_id])
+  @option3 = Option.create(option: params["option3"], question_id: @question.id, survey_id: params[:survey_id])
   erb :'survey/_question', layout: false
 end
